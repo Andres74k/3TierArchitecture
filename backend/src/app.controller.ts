72c3970ca12d851e9850c4a3dccffa6fdf91696d
Andres74k <74391741+Andres74k@users.noreply.github.com>
2026-02-21 19:58:31 -0600

    Add files via upload

---
+++ backend/src/app.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import * as os from 'os';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return `Hello from backend: ${os.hostname()}`;
+  }
+}
