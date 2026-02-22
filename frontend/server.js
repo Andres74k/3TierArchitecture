e041398d2d4cef806a4299500465bc787af12055
Andres74k <74391741+Andres74k@users.noreply.github.com>
2026-02-21 18:10:39 -0600

    Create server.js

---
+++ frontend/server.js
@@ -0,0 +1,32 @@
+const fastify = require('fastify')({ logger: true });
+const http = require('http');
+
+const APP_ALB = process.env.APP_ALB_DNS || "missing-alb";
+
+
+fastify.get('/', async (request, reply) => {
+  return `
+    <h1>Frontend (Fastify)</h1>
+    <h2 id="backend">Loading...</h2>
+
+    <script>
+      fetch('/backend')
+        .then(r => r.text())
+        .then(t => document.getElementById("backend").innerText = t)
+        .catch(() => document.getElementById("backend").innerText = "Error contacting backend");
+    </script>
+  `;
+});
+
+fastify.get('/backend', async (request, reply) => {
+  return new Promise((resolve, reject) => {
+    http.get(`http://${APP_ALB}`, res => {
+      let data = '';
+
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => resolve(data));
+    }).on('error', reject);
+  });
+});
+
+fastify.listen({ port: 3000, host: '0.0.0.0' });
