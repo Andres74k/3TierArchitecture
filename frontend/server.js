53654fe93baa11572e18e252c588b64f36f3624b
Andres74k <74391741+Andres74k@users.noreply.github.com>
2026-02-21 18:52:04 -0600

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,7 @@ const fastify = require('fastify')({ logger: true });
 const http = require('http');
 
 const APP_ALB = process.env.APP_ALB_DNS || "missing-alb";
+const AZ = process.env.AZ || "missing-AZ";
 
 
 fastify.get('/', async (request, reply) => {
@@ -9,7 +10,7 @@ fastify.get('/', async (request, reply) => {
     <h1>Frontend (Fastify)</h1>
     <br>
     <h1>Instance is running in:</h1>
-    <h2>$AZ</h2>
+    <h2 id="AZ">Loading...</h2>
     <h2 id="backend">Loading...</h2>
 
     <script>
@@ -17,6 +18,11 @@ fastify.get('/', async (request, reply) => {
         .then(r => r.text())
         .then(t => document.getElementById("backend").innerText = t)
         .catch(() => document.getElementById("backend").innerText = "Error contacting backend");
+
+      fetch('/AZ')
+        .then(r => r.text())
+        .then(t => document.getElementById("AZ").innerText = t)
+        .catch(() => document.getElementById("AZ").innerText = "Error could not get AZ");
     </script>
   `;
 });
@@ -30,6 +36,10 @@ fastify.get('/backend', async (request, reply) => {
       res.on('end', () => resolve(data));
     }).on('error', reject);
   });
+});
+
+fastify.get('/AZ', async (request, reply) => {
+  return AZ;
 });
 
 fastify.listen({ port: 3000, host: '0.0.0.0' });
